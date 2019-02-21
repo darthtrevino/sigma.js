d941fdbb3d731cfd06e153ed53f11292e05a9e05
Chris Trevino <darthtrevino@users.noreply.github.com>
2019-02-21 13:20:59 -0800

    Remove Grunt, migrate remaining tests to Jest (#11)
    * move plugin tests to jest
    
    * linting correction
    
    * Remove grunt

---
+++ src/plugins/sigma.plugins.filter/__tests__/filter.spec.js
@@ -0,0 +1,376 @@
+import sigma from "../../../core/index";
+import plugin from "../index";
+
+plugin(sigma);
+
+/* eslint-disable no-func-assign, no-shadow */
+describe("Plugin: sigma.plugins.filter", () => {
+  it("Custom graph methods", () => {
+    const myGraph = new sigma.classes.graph();
+    myGraph.read({
+      nodes: [
+        {
+          id: "n0",
+          label: "Node 0",
+          myNodeAttr: 123
+        },
+        {
+          id: "n1",
+          label: "Node 1"
+        },
+        {
+          id: "n2",
+          label: "Node 2"
+        },
+        {
+          id: "n3",
+          label: "Node 3"
+        }
+      ],
+      edges: [
+        {
+          id: "e0",
+          source: "n0",
+          target: "n1",
+          myEdgeAttr: 123
+        },
+        {
+          id: "e1",
+          source: "n1",
+          target: "n2"
+        },
+        {
+          id: "e2",
+          source: "n1",
+          target: "n3"
+        },
+        {
+          id: "e3",
+          source: "n2",
+          target: "n3"
+        }
+      ]
+    });
+
+    expect(myGraph.adjacentNodes("n0")).toEqual(
+      [myGraph.nodes("n1")],
+      '"adjacentNodes" returns the adjacent nodes of a specified node'
+    );
+
+    expect(myGraph.adjacentEdges("n0")).toEqual(
+      [myGraph.edges("e0")],
+      '"adjacentEdges" returns the adjacent edges of a specified node'
+    );
+  });
+
+  it("API", () => {
+    const s = new sigma();
+    const graph = {
+      nodes: [
+        {
+          id: "n0",
+          label: "Node 0",
+          myNodeAttr: 0
+        },
+        {
+          id: "n1",
+          label: "Node 1",
+          myNodeAttr: 1
+        },
+        {
+          id: "n2",
+          label: "Node 2",
+          myNodeAttr: 2
+        },
+        {
+          id: "n3",
+          label: "Node 3",
+          myNodeAttr: -1
+        },
+        {
+          id: "n4",
+          label: "Node 4"
+        }
+      ],
+      edges: [
+        {
+          id: "e0",
+          source: "n0",
+          target: "n1",
+          myEdgeAttr: 0
+        },
+        {
+          id: "e1",
+          source: "n1",
+          target: "n2",
+          myEdgeAttr: 1
+        },
+        {
+          id: "e2",
+          source: "n1",
+          target: "n3",
+          myEdgeAttr: 2
+        },
+        {
+          id: "e3",
+          source: "n2",
+          target: "n3",
+          myEdgeAttr: -1
+        },
+        {
+          id: "e4",
+          source: "n0",
+          target: "n0"
+        }
+      ]
+    };
+
+    // Initialize the filter:
+    const filter = new sigma.plugins.filter(s);
+
+    s.graph.read(graph);
+
+    // helper function
+    function hiddenNodes() {
+      return s.graph.nodes().filter(n => n.hidden);
+    }
+
+    // helper function
+    function hiddenEdges() {
+      return s.graph.edges().filter(e => e.hidden);
+    }
+
+    // Show non-isolated nodes only
+    function degreePredicate(n) {
+      return this.degree(n.id) > 0;
+    }
+
+    // Show edges without the myEdgeAttr attribute or with myEdgeAttr > 1
+    function myEdgeAttrPredicate(e) {
+      return e.myEdgeAttr === undefined || e.myEdgeAttr > 1;
+    }
+
+    // Register the filter
+    filter.nodesBy(degreePredicate, "degree");
+
+    expect(hiddenNodes()).toEqual(
+      [],
+      'A "nodesBy" filter is not applied automatically'
+    );
+
+    // Apply the filter
+    filter.apply();
+
+    expect(hiddenNodes()).toEqual(
+      [s.graph.nodes("n4")],
+      '"apply" applies a nodesBy filter'
+    );
+
+    // Undo this filter
+    filter.undo("degree").apply();
+
+    expect(hiddenNodes()).toEqual([], '"undo(a)" undoes the specified filter');
+
+    // Register another filter
+    filter.neighborsOf("n0").apply();
+
+    expect(hiddenNodes()).toEqual(
+      [s.graph.nodes("n2"), s.graph.nodes("n3"), s.graph.nodes("n4")],
+      '"neighborsOf" hides all nodes which are not linked to the specified node'
+    );
+
+    // Undo all filters
+    filter.undo().apply();
+
+    // Register an edge filter
+    filter.edgesBy(myEdgeAttrPredicate).apply();
+
+    expect(hiddenEdges()).toEqual(
+      [s.graph.edges("e0"), s.graph.edges("e1"), s.graph.edges("e3")],
+      '"apply" applies an edgesBy filter'
+    );
+
+    // Register two filters and apply them
+    filter
+      .nodesBy(degreePredicate)
+      .edgesBy(myEdgeAttrPredicate)
+      .apply();
+
+    // Undo all filters
+    filter.undo().apply();
+
+    expect(hiddenNodes().concat(hiddenEdges())).toEqual(
+      [],
+      "All filters are undone at once"
+    );
+
+    // Register two filters and apply them
+    filter
+      .nodesBy(degreePredicate, "degree")
+      .edgesBy(myEdgeAttrPredicate, "attr")
+      .apply();
+
+    expect(filter.export().map(o => o.key)).toEqual(
+      ["degree", "attr"],
+      "The filters chain is exported"
+    );
+
+    // Clear the filters chain
+    filter.clear();
+
+    expect(filter.export()).toEqual([], "The filters chain is cleared");
+    // Undo all filters
+    filter.undo().apply();
+
+    // nodesBy X > undo > nodesBy Y > apply
+    filter
+      .nodesBy(degreePredicate, "degree0")
+      .undo()
+      .nodesBy(function degree(n) {
+        return this.degree(n.id) > 1;
+      }, "degree1")
+      .apply();
+
+    expect(hiddenNodes()).toEqual(
+      [s.graph.nodes("n4")],
+      '"undo" undoes the filters before it in the chain, and not the filters after it'
+    );
+
+    // Call "apply" multiple times
+    filter.apply().apply();
+
+    expect(hiddenNodes()).toEqual(
+      [s.graph.nodes("n4")],
+      '"apply" is called multiple times'
+    );
+
+    // Call "undo" with multiple arguments
+    filter
+      .nodesBy(degreePredicate, "degree0")
+      .undo("degree0", "degree1")
+      .apply();
+
+    expect(hiddenNodes()).toEqual(
+      [],
+      '"undo" is called with multiple arguments'
+    );
+
+    // Import an empty chain
+    filter.import([]);
+
+    expect(filter.export().length).toEqual(0, "The empty chain is imported");
+
+    // Import a chain of filters
+    const chain = [
+      {
+        key: "my-filter",
+        predicate: degreePredicate,
+        processor: "filter.processors.nodes"
+      }
+    ];
+
+    filter.import(chain).apply();
+
+    expect(
+      filter.export().map(({ key, predicate, processor }) => ({
+        key,
+        predicate: predicate.toString(),
+        processor
+      }))
+    ).toEqual(
+      [
+        {
+          key: "my-filter",
+          predicate: degreePredicate.toString(),
+          processor: "filter.processors.nodes"
+        }
+      ],
+      "The filters chain is imported"
+    );
+
+    // export > import > export
+    const dumpedChain = filter.import(filter.export()).export();
+
+    expect(
+      chain.map(o => {
+        return {
+          key: o.key,
+          predicate: o.predicate.toString(),
+          processor: o.processor
+        };
+      })
+    ).toEqual(
+      dumpedChain.map(o => {
+        return {
+          key: o.key,
+          predicate: o.predicate.toString(),
+          processor: o.processor
+        };
+      }),
+      "The exported filters chain is imported"
+    );
+
+    // check chain duplication
+    filter.clear();
+
+    expect(dumpedChain.length).toEqual(
+      1,
+      "The exported chain is a deep copy of the internal chain"
+    );
+
+    // check chain duplication
+    filter.import(chain);
+    chain.length = 0;
+    degreePredicate = null;
+
+    expect(
+      filter.export().map(o => {
+        return {
+          key: o.key,
+          predicate: o.predicate.toString().replace(/\s+/g, " "),
+          processor: o.processor
+        };
+      })
+    ).toEqual(
+      [
+        {
+          key: "my-filter",
+          predicate: function degreePredicate(n) {
+            return this.degree(n.id) > 0;
+          }
+            .toString()
+            .replace(/\s+/g, " "),
+          processor: "filter.processors.nodes"
+        }
+      ],
+      "The internal chain is a deep copy of the imported chain"
+    );
+
+    function noop() {}
+
+    expect(() => filter.nodesBy(noop, 5)).toThrow(
+      /The filter key "5" must be a string./,
+      '"nodesBy" with a wrong key type throws an error.'
+    );
+
+    expect(() => filter.edgesBy(noop, "")).toThrow(
+      /The filter key must be a non-empty string./,
+      '"edgesBy" with a wrong key type throws an error.'
+    );
+
+    expect(() => filter.neighborsOf(0)).toThrow(
+      /The node id "0" must be a string./,
+      '"neighborsOf" with a wrong node id type throws an error.'
+    );
+
+    expect(() => filter.neighborsOf("")).toThrow(
+      /The node id must be a non-empty string./,
+      '"neighborsOf" with a wrong node id type throws an error.'
+    );
+
+    expect(() => filter.nodesBy(noop, "a").edgesBy(noop, "a")).toThrow(
+      /The filter "a" already exists./,
+      "Registering two filters with the same key throws an error."
+    );
+  });
+});
---
+++ src/plugins/sigma.statistics.HITS/__tests__/hits.spec.js
@@ -0,0 +1,252 @@
+import sigma from "../../../core/index";
+import plugin from "../index";
+
+plugin(sigma);
+
+describe("Plugin: sigma.statistics.HITS", () => {
+  // These tests are based on testing plan
+  // https://docs.google.com/file/d/0BznZHkruvUX6WkFBSmdEMWFReU0/edit
+  // read plugin documentation for more info
+  // read also for more context: https://github.com/jacomyal/sigma.js/issues/309#issuecomment-47554156
+
+  it("Stats computation", () => {
+    const opts = {};
+    const settings = new sigma.classes.configurable(opts);
+
+    const graph1 = {
+      nodes: [{ id: "n1" }],
+      edges: []
+    };
+
+    const graph2 = {
+      nodes: [{ id: "n1" }, { id: "n2" }],
+      edges: [{ id: "e1", source: "n1", target: "n2" }]
+    };
+
+    const graph3 = {
+      nodes: [
+        { id: "n1" },
+        { id: "n2" },
+        { id: "n3" },
+        { id: "n4" },
+        { id: "n5" }
+      ],
+      edges: []
+    };
+
+    const graph4 = {
+      nodes: [
+        { id: "n1" },
+        { id: "n2" },
+        { id: "n3" },
+        { id: "n4" },
+        { id: "n5" }
+      ],
+      edges: [
+        { id: "e1", source: "n1", target: "n2" },
+        { id: "e2", source: "n1", target: "n3" },
+        { id: "e3", source: "n1", target: "n4" },
+        { id: "e4", source: "n1", target: "n5" },
+        { id: "e5", source: "n2", target: "n3" },
+        { id: "e6", source: "n2", target: "n4" },
+        { id: "e7", source: "n2", target: "n5" },
+        { id: "e8", source: "n3", target: "n4" },
+        { id: "e9", source: "n3", target: "n5" },
+        { id: "e10", source: "n4", target: "n5" }
+      ]
+    };
+
+    const graph5 = {
+      nodes: [
+        { id: "n1" },
+        { id: "n2" },
+        { id: "n3" },
+        { id: "n4" },
+        { id: "n5" },
+        { id: "n6" }
+      ],
+      edges: [
+        { id: "e1", source: "n1", target: "n2" },
+        { id: "e2", source: "n1", target: "n3" },
+        { id: "e3", source: "n1", target: "n4" },
+        { id: "e4", source: "n1", target: "n5" },
+        { id: "e5", source: "n1", target: "n6" }
+      ]
+    };
+
+    const graph6 = {
+      nodes: [{ id: "n1" }, { id: "n2" }, { id: "n3" }],
+      edges: [
+        { id: "e1", source: "n1", target: "n1" },
+        { id: "e2", source: "n1", target: "n2" },
+        { id: "e3", source: "n2", target: "n3" },
+        { id: "e4", source: "n3", target: "n3" }
+      ]
+    };
+
+    const graph7 = {
+      nodes: [
+        { id: "n1" },
+        { id: "n2" },
+        { id: "n3" },
+        { id: "n4" },
+        { id: "n5" }
+      ],
+      edges: [
+        { id: "e1", source: "n1", target: "n4" },
+        { id: "e2", source: "n1", target: "n5" },
+        { id: "e3", source: "n2", target: "n4" },
+        { id: "e4", source: "n2", target: "n5" },
+        { id: "e5", source: "n3", target: "n4" },
+        { id: "e6", source: "n3", target: "n5" }
+      ]
+    };
+
+    const graph8 = {
+      nodes: [
+        { id: "n1" },
+        { id: "n2" },
+        { id: "n3" },
+        { id: "n4" },
+        { id: "n5" },
+        { id: "n6" }
+      ],
+      edges: [
+        { id: "e1", source: "n1", target: "n2" },
+        { id: "e2", source: "n1", target: "n3" },
+        { id: "e3", source: "n1", target: "n4" },
+        { id: "e4", source: "n1", target: "n5" },
+        { id: "e5", source: "n1", target: "n6" }
+      ]
+    };
+
+    const graph9 = {
+      nodes: [
+        { id: "n1" },
+        { id: "n2" },
+        { id: "n3" },
+        { id: "n4" },
+        { id: "n5" },
+        { id: "n6" }
+      ],
+      edges: [
+        { id: "e1", source: "n2", target: "n1" },
+        { id: "e2", source: "n3", target: "n1" },
+        { id: "e3", source: "n4", target: "n1" },
+        { id: "e4", source: "n5", target: "n1" },
+        { id: "e5", source: "n3", target: "n6" },
+        { id: "e6", source: "n4", target: "n6" },
+        { id: "e7", source: "n5", target: "n6" }
+      ]
+    };
+
+    const graph10 = {
+      nodes: [
+        { id: "n1" },
+        { id: "n2" },
+        { id: "n3" },
+        { id: "n4" },
+        { id: "n5" },
+        { id: "n6" }
+      ],
+      edges: [
+        { id: "e1", source: "n1", target: "n5" },
+        { id: "e2", source: "n2", target: "n5" },
+        { id: "e3", source: "n3", target: "n5" },
+        { id: "e4", source: "n4", target: "n5" },
+        { id: "e5", source: "n5", target: "n6" }
+      ]
+    };
+
+    // Initialize the graph:
+    const myGraph = new sigma.classes.graph(settings);
+
+    // HITS:
+    // ******
+
+    myGraph.read(graph1);
+
+    let stats = myGraph.HITS(true);
+
+    expect(stats.n1.authority === 1).toBeTruthy();
+    expect(stats.n1.hub === 1).toBeTruthy();
+
+    myGraph.clear();
+    myGraph.read(graph2);
+
+    stats = myGraph.HITS(true);
+
+    expect(stats.n1.authority).toEqual(0.5, "test 2");
+
+    myGraph.clear();
+    myGraph.read(graph3);
+
+    stats = myGraph.HITS(true);
+
+    expect(stats.n2.hub === 0.2).toBeTruthy();
+    expect(stats.n3.authority === 0.2).toBeTruthy();
+
+    myGraph.clear();
+    myGraph.read(graph4);
+
+    stats = myGraph.HITS(true);
+
+    expect(stats.n1.hub).toEqual(0.19999999999999998, "test 4.1");
+    expect(stats.n5.authority).toEqual(0.19999999999999998, "test 4.2");
+
+    myGraph.clear();
+    myGraph.read(graph5);
+
+    stats = myGraph.HITS(true);
+
+    expect(stats.n1.hub > stats.n3.hub).toBeTruthy();
+    expect(stats.n1.authority > stats.n4.authority).toBeTruthy();
+
+    myGraph.clear();
+    myGraph.read(graph6);
+
+    stats = myGraph.HITS(true);
+
+    expect(stats.n2.hub > stats.n1.hub).toBeTruthy();
+
+    myGraph.clear();
+    myGraph.read(graph7);
+
+    stats = myGraph.HITS();
+
+    expect(stats.n1.hub).toEqual(0.3333333333333333, "test 7.1");
+    expect(stats.n4.hub).toEqual(0, "test 7.2");
+    expect(stats.n2.authority).toEqual(0, "test 7.3");
+    expect(stats.n5.authority).toEqual(0.5, "test 7.4");
+
+    myGraph.clear();
+    myGraph.read(graph8);
+
+    stats = myGraph.HITS();
+
+    expect(stats.n1.hub).toEqual(1, "test 8.1");
+    expect(stats.n1.authority).toEqual(0, "test 8.2");
+    expect(stats.n3.hub).toEqual(0, "test 8.3");
+    expect(stats.n3.authority).toEqual(0.2, "test 8.4");
+
+    myGraph.clear();
+    myGraph.read(graph9);
+
+    stats = myGraph.HITS();
+
+    expect(stats.n3.hub).toEqual(stats.n5.hub, "test 9.1");
+    expect(stats.n3.hub > stats.n2.hub).toBeTruthy();
+    expect(stats.n1.authority > stats.n6.authority).toBeTruthy();
+    expect(stats.n6.hub).toEqual(0, "test 9.4");
+    expect(stats.n3.authority).toEqual(0, "test 9.5");
+
+    myGraph.clear();
+    myGraph.read(graph10);
+
+    stats = myGraph.HITS();
+
+    expect(stats.n1.hub).toEqual(stats.n3.hub, "test 10.1");
+    expect(stats.n1.hub > stats.n5.hub).toBeTruthy();
+    expect(stats.n5.authority > stats.n6.authority).toBeTruthy();
+  });
+});
