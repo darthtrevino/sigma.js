49f4b1dd64151d1da39f80f2dc519ceb676dcfb9
Chris Trevino <darthtrevino@gmail.com>
2019-02-21 16:50:29 -0800

    move middleware functions to typescript

---
+++ src/core/domain/middleware/copy.ts
@@ -0,0 +1,19 @@
+/**
+ * This middleware will just copy the graphic properties.
+ *
+ * @param {?string} readPrefix  The read prefix.
+ * @param {?string} writePrefix The write prefix.
+ */
+export default function copy(readPrefix: string, writePrefix: string) {
+  if (`${writePrefix}` === `${readPrefix}`) return;
+
+  this.graph.nodes().forEach(node => {
+    node[`${writePrefix}x`] = node[`${readPrefix}x`];
+    node[`${writePrefix}y`] = node[`${readPrefix}y`];
+    node[`${writePrefix}size`] = node[`${readPrefix}size`];
+  });
+
+  this.graph.edges().forEach(edge => {
+    edge[`${writePrefix}size`] = edge[`${readPrefix}size`];
+  });
+}
